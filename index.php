fe4752256345680d0152e8d2b3c4c498549132b3
BlackBashi <arlindomauricio2013sk8@gmail.com>
2019-07-02 16:52:01 -0400

    Adicionamos novas funçõe, metodos de lista

--- index.php
+++ index.php
@@ -2,9 +2,31 @@
 
 require_once ("config.php");
 
-$root = new Usuario();
 
-$root->loadById(4);
+//carrega um usuário apenas
+//$root = new Usuario();
 
-echo $root;
+//$root->loadById(4);
+
+//echo $root;
+
+
+//lista de usuarios:
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+
+//Carrega uma lista de Usuarios buscando pelo login:
+
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+
+//carrega um usuario usando o login e a senha:
+$usuario = new Usuario();
+$usuario->login("Cleito", "Clei123");
+
+echo $usuario;
 ?>
